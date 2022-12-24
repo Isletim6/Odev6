c1130c93f0a0499c1ca6e88da4324f634998b508
salimdemiray <salim_demiray@hotmail.com>
2022-12-24 22:23:23 +0300

    Processlerin Dosyadan Okunması için Hazırlanan İçi boş şablon.

---
+++ src/Helper/DosyaProcessOku.java
@@ -0,0 +1,17 @@
+package Helper;
+
+import java.util.ArrayList;
+
+public class DosyaProcessOku {
+
+	public DosyaProcessOku() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ArrayList<Process> DosyadanOku(String fileName) {
+		var processListesi=new  ArrayList<Process>();
+		
+		return processListesi;			
+	}
+
+}
