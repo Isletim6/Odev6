7ff7cfeed1ebde33a8b215379d9125d537df2db2
Buğra Tezcan <32716552+tezcanbugra@users.noreply.github.com>
2022-12-24 23:26:58 +0300

    Update FcfsKuyruk.java
    FcfsKuyruk implementation

--- src/Kuyruklar/FcfsKuyruk.java
+++ src/Kuyruklar/FcfsKuyruk.java
@@ -1,5 +1,6 @@
 package Kuyruklar;
 
+import java.util.ArrayList;
 
 public class FcfsKuyruk extends Kuyruk {
 
@@ -10,13 +11,25 @@ public class FcfsKuyruk extends Kuyruk {
 
 	@Override
 	public ProcessItem GetProcess(int kontrolSaniye)
-	{					
+	{			
+		ArrayList<ProcessItem> clone =new ArrayList<>();
+		clone.addAll(processListesi);						
+
+		clone.sort(ProcessItem.getKucukten());
+		
+		for(int i=0;i<clone.size();i++)
+		{
+			ProcessItem process=clone.get(i);
+			if(process.kalanSure==0 || process.gelisSuresi>kontrolSaniye) continue;
+			return process;			
+		}
+		
 		return null;
 	}
 
 	@Override
 	public void Isle(ProcessItem pp) {
-				
+		pp.ProcessIsle();		
 	}
 	
 }
