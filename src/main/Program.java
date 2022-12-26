5771dcb375e4f53e8f20c3a8abdabf497e0b3513
Ahmet Iyidogan <40362010+ahmet-github@users.noreply.github.com>
2022-12-26 22:44:51 +0300

    Update Program.java
    last_update

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,7 +1,11 @@
 package main;
 
-import Helper.ColorWrite;
-import Helper.Renkler;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import Helper.DosyaProcessOku;
+import Kuyruklar.GorevlendiriciKuyruk;
+import Kuyruklar.ProcessItem;
 
 public class Program {
 
@@ -11,7 +15,28 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-ColorWrite.Write(Renkler.Kirmizi, "İlk Commit");
+
+		if (args.length == 0) {
+			System.out.println("programi kullanmak için programa dosya adını arguman olarak yazınız");
+			return;
+		}
+		ArrayList<ProcessItem> process = new ArrayList<>();
+		try {
+			process = DosyaProcessOku.DosyadanOku(args[0]);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		GorevlendiriciKuyruk kuyruk = new GorevlendiriciKuyruk();
+
+		for (var pc : process) {
+			kuyruk.KuyrugaEkle(pc);
+		}		
+		
+		for (int i = 0; i < 30; i++) {
+			kuyruk.Run(i);
+		}		
 	}
 
 }
