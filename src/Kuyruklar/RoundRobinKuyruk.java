cd7164686821607a222cab3c0a3e0d245e2f89f3
fsup47 <58032459+fsup47@users.noreply.github.com>
2022-12-28 20:32:42 +0300

    RoundRobin.java yorum ekle

--- src/Kuyruklar/RoundRobinKuyruk.java
+++ src/Kuyruklar/RoundRobinKuyruk.java
@@ -1,21 +1,22 @@
+// Kuyruk sınıfını ve kütüphaneyi tanıladık
 package Kuyruklar;
 
 import java.util.ArrayList;
-
+//Round-robin için sınıf oluşturduk.
 public class RoundRobinKuyruk extends Kuyruk {
-
+	// TurCalisanlari nesnesini oluşturduk.
 	ArrayList<ProcessItem> TurCalisanlari;
-
+	// Öncelik parametresini çağırdık.
 	public RoundRobinKuyruk(int oncelik) {
 		super(oncelik);
 		// TODO Auto-generated constructor stub
 		TurCalisanlari = new ArrayList<ProcessItem>();
 		
 	}	
-	
+	// klon dizi oluşturduk ve süreleri kloana aktardık 
 	ArrayList<ProcessItem> zamaniGelmisProcessler(int kontrolSaniye) {
 		var clone = new ArrayList<ProcessItem>();
-
+	// Süreye bağlı olarak  kalansüresi 0'a eşit veya geliş zamını kontrol edilen zamandan büyük olunca ekler.
 		for (int i = 0; i < processListesi.size(); i++) {
 		     ProcessItem process = processListesi.get(i);
 		     if (process.kalanSure == 0 || process.gelisSuresi > kontrolSaniye)
@@ -25,24 +26,25 @@ public class RoundRobinKuyruk extends Kuyruk {
 		return clone;
 	}
 
-	
+	// Process liste kontrol, sıralama ve process zamana göre devam etme.
 	public ProcessItem GetProcess(int kontrolSaniye)
-	{		
+	{	
+		//yeni nesne oluşturduk ve tüm öğeleri ekledik.
 		ArrayList<ProcessItem> clone = new ArrayList<ProcessItem>();
 		clone.addAll(processListesi);
-
+		// Zamanı gelmiş processleri calisabilecek processlere aktardık.
 		var calisabilecekProcessler = zamaniGelmisProcessler(kontrolSaniye);
-
+		// Eğer oluşturulmuşsa listeyi temizliyoruz.
 		if (HepsiCalisti(calisabilecekProcessler))
 		   TurCalisanlari.clear();
-
+		// Listeyi sıraladık.
 		clone.sort(ProcessItem.getKucukten());
-
+		// Kalan zaman 0'a eşit ya da geliş zamanı kontrol edilen zamandan büyükse devam eder.
 		for (int i = 0; i < clone.size(); i++) {
 			ProcessItem process = clone.get(i);
 			if (process.kalanSure == 0 || process.gelisSuresi > kontrolSaniye)
 			    continue;
-
+		// TurCalisanlari listesinde process kontrol ediyoruz bu listedeyse devam ediyoruz.
 			if (TurCalisanlari.contains(process))
 			    continue;
 
@@ -50,19 +52,20 @@ public class RoundRobinKuyruk extends Kuyruk {
 		}
 		return null;
 	}
-	
-	boolean HepsiCalisti(ArrayList<ProcessItem> calisabilecekProcessler) {
 
+	//  Listedeki herbir processin Turcalisanları dizisinde bulunup (true) bulunmadığına(false) bakıyoruz
+	boolean HepsiCalisti(ArrayList<ProcessItem> calisabilecekProcessler) {
+		// döngü ile herbirini kontrol ediyoruz. Bulunmuyorsa false.
 		for (ProcessItem pp : calisabilecekProcessler) {
 		     if (!TurCalisanlari.contains(pp))
 			return false;
 		}
-
+		// bulunuyorsa true döndürüyoruz.
 		return true;
 	}
 
 
-
+	// Processs'in kalan zamanı 0 dan büyük varsa TurCalisanlari na ekler.
 	@Override
 	public void Isle(ProcessItem pp) {				
 		pp.ProcessIsle();
